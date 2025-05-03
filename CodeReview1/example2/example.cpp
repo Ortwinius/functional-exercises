a9b8efd978e713f5edd40f815ae0e170ffbe1921
Ortwinius <ortwin.baldauf@gmail.com>
2025-05-03 14:46:41 +0200

    Small changes to exc

--- CodeReview1/example2/example.cpp
+++ CodeReview1/example2/example.cpp
@@ -1,47 +1,44 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 #include <sstream>
+#include <string>
 
 using namespace std;
 
-struct CubeProps
-{
-    int volume = 0;
-    int area = 0;
-    double diagonal = 0;
-    double ru = 0;
-    double ri = 0;
+struct CubeProps {
+  int volume = 0;
+  int area = 0;
+  double diagonal = 0;
+  double ru = 0;
+  double ri = 0;
 };
 
-const CubeProps wuerfel (const int& a)
-{
-    CubeProps cube;
-	cube.volume = pow(a,3);
-	cube.area = 6*pow(a,2);
-	cube.diagonal =a*sqrt(3);
-	cube.ru = (double)a/2*sqrt(3);
-	cube.ri = (double)a/2;
-    return cube;
+const CubeProps wuerfel(const int &a) {
+  CubeProps cube;
+  cube.volume = pow(a, 3);
+  cube.area = 6 * pow(a, 2);
+  cube.diagonal = a * sqrt(3);
+  cube.ru = (double)a / 2 * sqrt(3);
+  cube.ri = (double)a / 2;
+  return cube;
 }
 
-string formatResults(const CubeProps& result)
-{
-    stringstream output;
-    output << "Volumen: " << result.volume << "\n";
-    output << "Oberfläche: " << result.area << "\n";
-    output << "Diagonale: " << result.diagonal << "\n";
-    output << "Inkreisradius: " << result.ri << "\n";
-    output << "Umkreisradius: " << result.ru << "\n";
-    return output.str();
+const string formatResultsPure(const CubeProps &result) {
+  stringstream output;
+  output << "Volumen: " << result.volume << "\n";
+  output << "Oberfläche: " << result.area << "\n";
+  output << "Diagonale: " << result.diagonal << "\n";
+  output << "Inkreisradius: " << result.ri << "\n";
+  output << "Umkreisradius: " << result.ru << "\n";
+  return output.str();
 }
-int
-main (int argc, char *argv[])
-{
+
+void printPure(const string &text) { cout << text; }
+
+int main(int argc, char *argv[]) {
   int a = 2;
-  CubeProps result = wuerfel (a);
-  cout << "Volumen:" << result.volume << " Oberfläche" 
-        << result.area << " Diagonale:" << result.diagonal 
-        << " Inkreisradius:" << result.ri << "Umkreisradius:" << result.ru << endl;
-  /*cout << "Volumen:" << V << " Oberfläche:"<<A << " Diagonale:" << d << " Inkreisradius:" << ri << " Umkreisradius:" <<ru << endl;*/
+  CubeProps result = wuerfel(a);
+  printPure(formatResultsPure(result));
+
   return 0;
 }
