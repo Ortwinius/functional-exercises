d7fe0e339773bfb287ed1c66baccd6b0f92a965c
Ortwinius <ortwin.baldauf@gmail.com>
2025-05-03 14:42:01 +0200

    Refactored first two exercises

--- CodeReview1/example1/example.cpp
+++ CodeReview1/example1/example.cpp
@@ -1,59 +1,44 @@
 // Example1: Functional version by Ortwin Baldauf
+#include <cmath>
 #include <iostream>
 #include <sstream>
-#include <cmath>
 
 using namespace std;
 
 // Changes: Replaced global vars with local struct properties
-struct CubeProps
-{
-    int volume = 0;
-    int area = 0;
-    double diagonal = 0;
+struct CubeProps {
+  int volume = 0;
+  int area = 0;
+  double diagonal = 0;
 };
 
 // pure function which takes three constant parameters
 // and returns a new CubeProps without changing the original vals
-const CubeProps quader (const int a, const int b, const int c)
-{
-    CubeProps cube;
-    cube.volume = a*b*c;
-    cube.area = 2*(a*b+a*c+b*c);
-    // potentially replace pow with a*a, b*b, c*c?
-	cube.diagonal = sqrt(pow(a,2)+pow(b,2)+pow(c,2));
+const CubeProps quader(const int a, const int b, const int c) {
+  CubeProps cube;
+  cube.volume = a * b * c;
+  cube.area = 2 * (a * b + a * c + b * c);
+  cube.diagonal = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2));
 
-    return cube;
+  return cube;
 }
 
-string formatResultsPure(const int* pValue)
-{
-    stringstream output;
-    output << "Adress: " << pValue << endl;
-    output << "Increment pointer adress and value pure: " << (pValue + 1) << " value: " << *pValue << endl;
-
-    return output.str();
+const string formatResultsPure(const CubeProps &cube) {
+  stringstream output;
+  output << "Volumen: " << cube.volume << "\n";
+  output << "Oberfläche: " << cube.area << "\n";
+  output << "Diagonale: " << cube.diagonal << "\n";
+  return output.str();
 }
 
-void printSomething(const string& text)
-{
-    cout << text;
-}
+void printPure(const string &text) { cout << text; }
 
-int
-main (int argc, char *argv[])
-{
-    int a = 2, b = 3, c = 4;
+int main(int argc, char *argv[]) {
+  int a = 2, b = 3, c = 4;
 
-    // initialize new cube and calculate properties via quader
-    CubeProps newCube = quader (a,b,c);
-    
-    // TODO this part is not pure because it relies on external I/O functions
-    /*cout << "Volumen:" << newCube.volume << " Oberfläche:"<< newCube.area << " Diagonale:" << newCube.diagonal << endl;*/
+  CubeProps newCube = quader(a, b, c);
 
-    int* pValue = new int(10);
-    printSomething(formatResultsPure(pValue));
-    delete pValue;
-    
-    return 0;
+  printPure(formatResultsPure(newCube));
+
+  return 0;
 }
