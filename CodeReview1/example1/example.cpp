c470a7dbab18782c079bfcf47eb0b8f0ecab5e0f
Ortwinius <ortwin.baldauf@gmail.com>
2025-05-04 13:51:29 +0200

    Added nr.1 fix

--- CodeReview1/example1/example.cpp
+++ CodeReview1/example1/example.cpp
@@ -5,40 +5,49 @@
 
 using namespace std;
 
-// Changes: Replaced global vars with local struct properties
-struct CubeProps {
-  int volume = 0;
-  int area = 0;
-  double diagonal = 0;
+class Result {
+private:
+  int a = 0;
+  int b = 0;
+  int c = 0;
+  int V = 0;
+  int A = 0;
+  float d = 0;
+
+  const int calcVolume(const int &a, const int &b, const int &c) {
+    return a * b * c;
+  }
+
+  const int calcArea(const int &a, const int &b, const int &c) {
+    return 2 * (a * b + a * c + b * c);
+  }
+
+  const float calcDiagonal(const int &a, const int &b, const int &c) {
+    return (float)sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2));
+  }
+
+public:
+  Result(const int &a, const int &b, const int &c)
+      : a(a), b(b), c(c), V(calcVolume(a, b, c)), A(calcArea(a, b, c)),
+        d(calcDiagonal(a, b, c)) {}
+
+  // const at the end to specify that it doesnt alter any member variables
+  const string formatResults() const {
+    stringstream output;
+
+    output << "Volumen: " << V << "\n";
+    output << "Oberfläche: " << A << "\n";
+    output << "Diagonale: " << d << "\n";
+    return output.str();
+  }
 };
-
-// pure function which takes three constant parameters
-// and returns a new CubeProps without changing the original vals
-const CubeProps quader(const int a, const int b, const int c) {
-  CubeProps cube;
-  cube.volume = a * b * c;
-  cube.area = 2 * (a * b + a * c + b * c);
-  cube.diagonal = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2));
-
-  return cube;
-}
-
-const string formatResults(const CubeProps &cube) {
-  stringstream output;
-  output << "Volumen: " << cube.volume << "\n";
-  output << "Oberfläche: " << cube.area << "\n";
-  output << "Diagonale: " << cube.diagonal << "\n";
-  return output.str();
-}
 
 void printText(const string &text) { cout << text; }
 
 int main(int argc, char *argv[]) {
-  int a = 2, b = 3, c = 4;
-
-  CubeProps newCube = quader(a, b, c);
-
-  printText(formatResults(newCube));
+  const int a = 2, b = 3, c = 4;
+  Result res(a, b, c);
+  printText(res.formatResults());
 
   return 0;
 }
