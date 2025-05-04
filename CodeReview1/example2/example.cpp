d9b34e36a9185af94a7886875dcabcb49277162a
Ortwinius <ortwin.baldauf@gmail.com>
2025-05-04 13:35:36 +0200

    fixed nr 2

--- CodeReview1/example2/example.cpp
+++ CodeReview1/example2/example.cpp
@@ -5,40 +5,49 @@
 
 using namespace std;
 
-struct CubeProps {
-  int volume = 0;
-  int area = 0;
-  double diagonal = 0;
-  double ru = 0;
-  double ri = 0;
+// TODO: rename to Wuerfel
+class Result {
+private:
+  int a = 0;
+  int V = 0;
+  int A = 0;
+  float d = 0;
+  float ru = 0;
+  float ri = 0;
+
+  const int calcVolume(const int &a) { return pow(a, 3); }
+
+  const int calcArea(const int &a) { return 6 * pow(a, 2); }
+
+  const float calcDiagonal(const int &a) { return a * sqrt(3); }
+
+  const float calcRU(const int &a) { return (float)a / 2 * sqrt(3); }
+
+  const float calcRI(const int &a) { return (float)a / 2; }
+
+public:
+  Result(const int &a)
+      : a(a), V(calcVolume(a)), A(calcArea(a)), ri(calcRI(a)), ru(calcRU(a)) {}
+
+  // const at the end to specify that it doesnt alter any member variables
+  const string formatResults() const {
+    stringstream output;
+
+    output << "Volumen: " << V << "\n";
+    output << "Oberfläche: " << A << "\n";
+    output << "Inkreisradius: " << ri << "\n";
+    output << "Umkreisradius: " << ru << "\n";
+
+    return output.str();
+  }
 };
 
-const CubeProps wuerfel(const int &a) {
-  CubeProps cube;
-  cube.volume = pow(a, 3);
-  cube.area = 6 * pow(a, 2);
-  cube.diagonal = a * sqrt(3);
-  cube.ru = (double)a / 2 * sqrt(3);
-  cube.ri = (double)a / 2;
-  return cube;
-}
-
-const string formatResultsPure(const CubeProps &result) {
-  stringstream output;
-  output << "Volumen: " << result.volume << "\n";
-  output << "Oberfläche: " << result.area << "\n";
-  output << "Diagonale: " << result.diagonal << "\n";
-  output << "Inkreisradius: " << result.ri << "\n";
-  output << "Umkreisradius: " << result.ru << "\n";
-  return output.str();
-}
-
-void printPure(const string &text) { cout << text; }
+void printText(const string &text) { cout << text; }
 
 int main(int argc, char *argv[]) {
-  int a = 2;
-  CubeProps result = wuerfel(a);
-  printPure(formatResultsPure(result));
+  const int a = 2;
+  Result res(a);
+  printText(res.formatResults());
 
   return 0;
 }
